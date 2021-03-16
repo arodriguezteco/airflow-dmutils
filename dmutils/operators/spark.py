b87ac4e81d23f6a4bd4dea2c412e56d3458706b6
Agustin Rodriguez <gus990@gmail.com>
2021-03-16 13:34:19 -0300

    move param defaults inside super().__init__()

--- dmutils/operators/spark.py
+++ dmutils/operators/spark.py
@@ -3,24 +3,23 @@ from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
 __all__ = ["LowSparkSubmitOperator"]
 
 class LowSparkSubmitOperator(SparkSubmitOperator):
-    def __init__(
-        self,
-        **kwargs
-    ):
-        super().__init__(**kwargs)
-        self.executor_cores="4",
-        self.executor_memory="8g",
-        self.num_executors="2",
-        self.master="yarn",
-        self.verbose=True,
-        self.yarn_queue="AIRFLOW",
-        self.spark_binary="/bin/spark-submit22",
-        self.conf=",".join(
-            [
-                "spark.dynamicAllocation.enabled=true",
-                "spark.dynamicAllocation.initialExecutors=2",
-                "spark.dynamicAllocation.minExecutors=2",
-                "spark.dynamicAllocation.maxExecutors=8",
-                "spark.shuffle.service.enabled=true",
-            ]
-        ),
+    def __init__(self, **kwargs):
+        super().__init__(
+            executor_cores="4",
+            executor_memory="8g",
+            num_executors="2",
+            master="yarn",
+            verbose=True,
+            yarn_queue="AIRFLOW",
+            spark_binary="/bin/spark-submit22",
+            conf=",".join(
+                [
+                    "spark.dynamicAllocation.enabled=true",
+                    "spark.dynamicAllocation.initialExecutors=2",
+                    "spark.dynamicAllocation.minExecutors=2",
+                    "spark.dynamicAllocation.maxExecutors=8",
+                    "spark.shuffle.service.enabled=true",
+                ]
+            )
+            **kwargs
+        )
