fe203d8320e509f461be01cab681df3ee92ff61c
Agustin Rodriguez <gus990@gmail.com>
2021-03-23 13:59:15 -0300

    import cluster config from module

--- dmutils/operators/spark.py
+++ dmutils/operators/spark.py
@@ -1,3 +1,4 @@
+from dmutils.config.cluster import LOW
 from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
 
 __all__ = ["LowSparkSubmitOperator"]
@@ -5,20 +6,5 @@ __all__ = ["LowSparkSubmitOperator"]
 
 class LowSparkSubmitOperator(SparkSubmitOperator):
     def __init__(self, **kwargs):
-        super().__init__(
-            executor_cores="4",
-            executor_memory="8g",
-            num_executors="2",
-            master="yarn",
-            verbose=True,
-            yarn_queue="AIRFLOW",
-            spark_binary="/bin/spark-submit",
-            conf={
-                "spark.dynamicAllocation.enabled": "true",
-                "spark.dynamicAllocation.initialExecutors": "2",
-                "spark.dynamicAllocation.minExecutors": "2",
-                "spark.dynamicAllocation.maxExecutors": "8",
-                "spark.shuffle.service.enabled": "true",
-            },
-            **kwargs
-        )
+        kwargs = kwargs.update(LOW)
+        super().__init__(**kwargs)
